3999d944c818d18eb5496a8ffac52e7f728e1dc7
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-04-20 15:23:02 -0500

    Update local.tf

--- local.tf
+++ local.tf
@@ -3,6 +3,6 @@ resource "null_resource" "login" {
     command = "aws eks --region ${var.region} update-kubeconfig --name ${var.cluster_name}"
     }
     provisioner "local-exec" {
-    command = "/tmp/kubectl get nodes"
+    command = "/usr/local/bin/kubectl get nodes"
   }
 }
