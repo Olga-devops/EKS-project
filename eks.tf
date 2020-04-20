02e938b771a390e0da4eeedb0b711fe9bc0d8cae
Olga Ojjeh <Olgaojjeh.com>
2020-04-20 12:35:18 -0500

    add

--- eks.tf
+++ eks.tf
@@ -12,7 +12,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token = data.aws_eks_cluster_auth.cluster.token
   load_config_file = false
-  version = "~> 1.9"
+  version = "~> 10.0.0"
 } 
 
 module "my-cluster" {
@@ -21,6 +21,7 @@ module "my-cluster" {
   cluster_version = var.cluster_version 
   subnets = ["${var.subnet1}", "${var.subnet2}"]
   vpc_id = var.vpc_id
+  
   worker_groups = [{ 
     instance_type = var.instance_type 
     asg_max_size = var.asg_max_size 
